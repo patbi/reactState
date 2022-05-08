738cc7508f4f96297c1d1021ee6c60f0cd834d78
patbi <biyagapatrick@gmail.com>
2022-05-08 16:40:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    {/*count += 1; // WRONG */}
+
+    setCount(count + 1); // CORRECT
+  };
+
+  const decrement = () => {
+    {/*count -= 1; // WRONG */}
+
+    setCount(count - 1); // CORRECT
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to my counter app</h1>
+      <p>The count is: {count} </p>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
